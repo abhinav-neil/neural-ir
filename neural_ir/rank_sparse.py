3b405c3368e44a7e6d91f9f76b5f63a483fc6073
Andrew Yates <andrew.yates@gmail.com>
2023-03-01 19:24:18 +0100

    rank_sparse.py fix

--- neural_ir/rank_sparse.py
+++ neural_ir/rank_sparse.py
@@ -41,48 +41,65 @@ tokenizer = AutoTokenizer.from_pretrained(args.checkpoint)
 vocabulary = tokenizer.vocab
 model = SparseBiEncoder.from_pretrained(args.checkpoint).to(args.device)
 model.eval()
-query_embs = []
-docs_embs = []
-doc_ids = []
-for idx in tqdm(
-    range(0, len(docs), args.bs), desc="Encoding documents", position=0, leave=True
-):
-    batch = docs[idx : idx + args.bs]
-    docs_texts = [e[1] for e in batch]
-    doc_ids.extend([e[0] for e in batch])
-    docs_inps = tokenizer(
-        docs_texts, truncation=True, padding=True, return_tensors="pt"
-    ).to(args.device)
-    with torch.cuda.amp.autocast(), torch.no_grad():
-        batch_embs = model.encode(**docs_inps).to("cpu")
-        docs_embs.append(batch_embs * 100)
-
-docs_embs = torch.cat(docs_embs, dim=0).to(torch.int32)
-json_docs = defaultdict(dict)
-for row, col in tqdm(
-    docs_embs.nonzero(), desc="Converting dense format to sparse format"
-):
-    doc_id = doc_ids[row]
-    term = tokenizer.convert_ids_to_tokens(col.item())
-    weight = docs_embs[row, col].item()
-    json_docs[doc_id][term] = weight
-
 
 d_path = Path("output/sparse/docs/")
 if not d_path.exists():
     d_path.mkdir(parents=True, exist_ok=True)
 
-with open(d_path / "docs.jsonl", "w") as f:
-    for doc_id in tqdm(
-        json_docs,
-        desc="Writing documents to output/sparse/docs in json format",
-        position=0,
-        leave=True,
-    ):
-        f.write(json.dumps({"id": doc_id, "vector": json_docs[doc_id]}) + "\n")
+doc_file = open(d_path / "docs.jsonl", "w")
 
-query_embs = []
-query_ids = []
+
+def write_doc_to_file(batch_embds, batch_doc_ids, output_file):
+    batch_embds = (batch_embds * 100).to(torch.int32)
+    k_values, k_indices = batch_embds.topk(400, dim=1)
+    for doc_id, token_ids, token_weights in zip(
+        batch_doc_ids, k_indices.tolist(), k_values.tolist()
+    ):
+        doc_json = {"id": doc_id, "vector": dict(zip(token_ids, token_weights))}
+        output_file.write(json.dumps(doc_json) + "\n")
+
+
+for idx in tqdm(
+    range(0, len(docs), args.bs), desc="Encoding documents", position=0, leave=True
+):
+    batch = docs[idx : idx + args.bs]
+    docs_texts = [e[1] for e in batch]
+    batch_doc_ids = [e[0] for e in batch]
+    docs_inps = tokenizer(
+        docs_texts, truncation=True, padding=True, return_tensors="pt"
+    ).to(args.device)
+    with torch.cuda.amp.autocast(), torch.no_grad():
+        batch_embs = model.encode(**docs_inps).to("cpu")
+        write_doc_to_file(batch_embs, batch_doc_ids, doc_file)
+doc_file.close()
+
+q_path = Path("output/sparse/queries/")
+if not q_path.exists():
+    q_path.mkdir(parents=True, exist_ok=True)
+query_file = open(q_path / "test.tsv", "w")
+
+
+def write_query_to_file(batch_embds, batch_query_ids, output_file):
+    batch_embds = (batch_embds * 100).to(torch.int32)
+    # repeat_queries = defaultdict(lambda: "")
+    k_values, k_indices = batch_embds.topk(400, dim=1)
+    for query_id, token_ids, token_weights in zip(
+        batch_query_ids, k_indices.tolist(), k_values.tolist()
+    ):
+        repeat_query = " ".join(
+            [
+                " ".join([str(token_id)] * token_weight)
+                for token_id, token_weight in zip(token_ids, token_weights)
+                if token_weight > 0
+            ]
+        )
+        output_file.write(f"{query_id}\t{repeat_query}\n")
+        # doc_json = {"id": doc_id, "vector": dict(zip(token_ids, token_weights))}
+        # output_file.write(json.dumps(doc_json) + "\n")
+        # for qid in repeat_queries:
+        # output_file.write(f"{qid}\t{repeat_queries[qid]}\n")
+
+
 for idx in tqdm(
     range(0, len(queries), args.bs),
     desc="Encoding queries and search",
@@ -91,28 +108,17 @@ for idx in tqdm(
 ):
     batch = queries[idx : idx + args.bs]
     query_texts = [e[1] for e in batch]
-    query_ids.extend([e[0] for e in batch])
+    batch_query_ids = [e[0] for e in batch]
     query_inps = tokenizer(
         query_texts, truncation=True, padding=True, return_tensors="pt"
     ).to(args.device)
     with torch.cuda.amp.autocast(), torch.no_grad():
         batch_query_embs = model.encode(**query_inps).to("cpu")
-    query_embs.append(batch_query_embs * 100)
-query_embs = torch.cat(query_embs, dim=0).to(torch.int32)
-repeat_queries = defaultdict(lambda: "")
-for row, col in query_embs.nonzero():
-    query_id = query_ids[row]
-    term = tokenizer.convert_ids_to_tokens(col.item())
-    weight = query_embs[row, col]
-    repeat_queries[query_id] = (
-        repeat_queries[query_id] + " " + " ".join([term] * weight)
-    )
-q_path = Path("output/sparse/queries/")
-if not q_path.exists():
-    q_path.mkdir(parents=True, exist_ok=True)
-with open(q_path / "test.tsv", "w") as f:
-    for qid in tqdm(repeat_queries, desc=f"Writing queries to {q_path}"):
-        f.write(f"{qid}\t{repeat_queries[qid]}\n")
+        write_query_to_file(batch_query_embs, batch_query_ids, query_file)
+query_file.close()
+# query_embs.append(batch_query_embs * 100)
+# query_embs = torch.cat(query_embs, dim=0).to(torch.int32)
+
 
 # python -m pyserini.index.lucene   --collection JsonVectorCollection   --input output/sparse/docs   --index test_index   --generator DefaultLuceneDocumentGenerator   --threads 12   --impact --pretokenized
 INDEX_COMMAND = """python -m pyserini.index.lucene
